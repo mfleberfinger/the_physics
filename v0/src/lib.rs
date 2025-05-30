11cf8a1fe1c2229a5aa0357aed936d4db8c6ab0b
Matthew <mfleberfinger@gmail.com>
2025-05-30 07:21:50 -0400

    Starting to write structs.

--- v0/src/lib.rs
+++ v0/src/lib.rs
@@ -13,6 +13,19 @@ mod tests {
     }
 }
 
-struct Simulation {
+// Using a tuple struct to wrap an i64 so the compiler treats Seconds as a
+//	distinct type. This is the "newtype pattern."
+/// Represents time, in seconds, within the simulation.
+pub struct Seconds(i64);
 
+pub struct Particle {
+	// TODO: add fields
+}
+
+/// Owns all of the data that constitutes a physics simulation. Provides methods
+/// for interacting with and running the simulation.
+pub struct Simulation {
+	tick_duration: Seconds,
+	particles: HashMap<Particle>,
+	// TODO: Continue adding fields.
 }
